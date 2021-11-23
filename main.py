3fa002817835e88639f6091b6bc3cef3465d4520
lluis <33216794+lluismlg@users.noreply.github.com>
2021-11-23 15:43:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def check_files(request):
    initial_dir = os.listdir("./")
-   second_dir = os.listdir("./files")
+   second_dir = os.listdir("./files/sites/default/files/special_topics/cn/")
    
    initial_dir = ' / '.join(initial_dir)
    second_dir = ' / '.join(second_dir)
